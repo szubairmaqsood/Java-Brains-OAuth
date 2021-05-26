528c11399c69c8b6b8c6d5b0cdb0399181f105e3
zubair-pc\zubair <szubair@gmail.com>
2021-05-26 14:41:22 +0500

    1.Application.preperties files is renamed to Application.yml 2.Dependency of O Auth is added in gradle file 3.InMain Programme file of @EnableOAuth2Sso annotation is added

--- Java-Brains-OAuth/build.gradle.kts
+++ Java-Brains-OAuth/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:2.4.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
--- Java-Brains-OAuth/src/main/kotlin/com/example/Java/Brains/OAuth/JavaBrainsOAuthApplication.kt
+++ Java-Brains-OAuth/src/main/kotlin/com/example/Java/Brains/OAuth/JavaBrainsOAuthApplication.kt
@@ -1,9 +1,11 @@
 package com.example.Java.Brains.OAuth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableOAuth2Sso
 class JavaBrainsOAuthApplication
 
 fun main(args: Array<String>) {
