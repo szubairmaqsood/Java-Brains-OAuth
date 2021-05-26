065e8eceab4f1b31e13cb40e47984b76a15deebc
zubair-pc\zubair <szubair@gmail.com>
2021-05-26 14:18:48 +0500

    Inital structure of Java Brains

---
+++ Java-Brains-OAuth/src/main/kotlin/com/example/Java/Brains/OAuth/JavaBrainsOAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Java.Brains.OAuth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaBrainsOAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<JavaBrainsOAuthApplication>(*args)
+}
---
+++ Java-Brains-OAuth/src/test/kotlin/com/example/Java/Brains/OAuth/JavaBrainsOAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Java.Brains.OAuth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaBrainsOAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
